238ce066aebee17618a600039b2896a47c8146d3
Ayush Kumar Mishra <ayushmishra1376@gmail.com>
2024-05-06 17:32:16 +0530

    Menu Added

---
+++ Capstone_ProjectP2/Assets/Scripts/SceneManagementPrj.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagementPrj : MonoBehaviour
+{
+    public void ExpOne() 
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+    public void ExpTwo() 
+    {
+        SceneManager.LoadScene(2);
+    }
+    
+    public void ExpThree() 
+    {
+        SceneManager.LoadScene(3);
+    }
+    
+    public void ExpFour() 
+    {
+        SceneManager.LoadScene(4);
+    }
+}
